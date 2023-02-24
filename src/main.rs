b954cbadf5eb4a67a9e6027d854cc1608683e815
Andrey Khalepov <w54661c@gmail.com>
2023-02-24 19:41:17 +1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use chrono::Local;
+use core::default::Default;
+use iced::widget::{Column, Text};
+use iced::{executor, Application, Command, Element, Renderer, Settings, Subscription, Theme};
+
+const FONT: &[u8] = include_bytes!("../fonts/Terminus.ttf");
+fn main() -> iced::Result {
+    App::run(Settings {
+        default_font: Some(FONT),
+        window: iced::window::Settings {
+            size: (980, 256),
+            visible: true,
+            resizable: false,
+            decorations: true,
+            always_on_top: true,
+            ..Default::default()
+        },
+        ..Default::default()
+    })?;
+    Ok(())
+}
+
+struct App {
+    time: String,
+}
+
+#[derive(Debug)]
+enum Message {
+    Tick,
+}
+
+impl Application for App {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        let date_time = Local::now();
+        (
+            Self {
+                time: format!("{}", date_time.format("%H:%M:%S")),
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("Настольные часы")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::Tick => {
+                self.time = format!("{}", Local::now().format("%H:%M:%S"));
+            }
+        }
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message, Renderer<Self::Theme>> {
+        let label = Text::new(&self.time).size(240);
+        Column::new().push(label).into()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        iced::time::every(std::time::Duration::from_millis(1000)).map(|_| Message::Tick)
+    }
+}
