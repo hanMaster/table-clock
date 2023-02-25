1e64124c7a4401653f51c78a6bccaf988634b714
Andrey Khalepov <w54661c@gmail.com>
2023-02-25 17:58:43 +1000

    hide logs

--- src/app.rs
+++ src/app.rs
@@ -30,10 +30,10 @@ impl Application for App {
 
     fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
         let config = confy::load(APP_NAME, None).unwrap_or_default();
-        println!("{:#?}", config);
-        let path =
-            confy::get_configuration_file_path(APP_NAME, None).expect("Failed to get config path");
-        println!("Config path: {:#?}", path);
+        // println!("{:#?}", config);
+        // let path =
+        //     confy::get_configuration_file_path(APP_NAME, None).expect("Failed to get config path");
+        // println!("Config path: {:#?}", path);
         let date_time = Local::now();
         (
             Self {
